a4b412bcdc3604ef16465e5959342eb9fcf99d09
Miguel <elperrodescarga@gmail.com>
2015-12-30 21:00:46 -0600

    Se elimino el estilo de los archivos index y los que estan en la carpeta buscar

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<link rel="stylesheet" type="text/css" href="css/estilo.css" />
+<!--<link rel="stylesheet" type="text/css" href="css/estilo.css" />-->
 <script type="text/javascript" src="js/jquery-2.1.1.js"></script>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Cuestionario</title>
@@ -10,7 +10,7 @@
 
 <body>
     <header>
-    <input type="checkbox"  value="123"  ></input> 
+    <!--<input type="checkbox"  value="123"  ></input> -->
     	<h1>Cuestionarios</h1>
         <nav>
         	<ul class="menu">
